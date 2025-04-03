b3ba16b9b39244b192a52a766b10745fa1291183
Simpaqt <simpaqt@yahoo.com>
2025-04-03 13:59:22 +0200

    bg

--- home/simpa/modules/sway/default.nix
+++ home/simpa/modules/sway/default.nix
@@ -103,7 +103,7 @@
     
     extraConfig = ''
       # Set wallpaper
-      output * bg ${pkgs.nixos-artwork.wallpapers.nineish-dark-gray.src} fill
+      output * bg ${~/Downloads/fox.png} fill
       
       # Font for window titles (if titlebars are enabled)
       font pango:JetBrains Mono 11
