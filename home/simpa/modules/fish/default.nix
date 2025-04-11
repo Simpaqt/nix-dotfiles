538971c405ee932b724641a48e8ce8d399ad146f
Simpaqt <simpaqt@yahoo.com>
2025-04-11 14:05:49 +0200

    laptop config

--- home/simpa/modules/fish/default.nix
+++ home/simpa/modules/fish/default.nix
@@ -72,12 +72,11 @@
        alias mail="neomutt"
 
        #nix alias
-       alias rebuild="sudo nixos-rebuild switch --flake ~/nixos#nixos"
+       alias rb="sudo nixos-rebuild switch --flake ~/nixos#nixos"
+       alias rbl="sudo nixos-rebuild switch --flake ~/nixos#laptop"
        alias hms="home-manager switch"
-       alias update="nix flake update ~/nixos#nixos"
+       alias update="sudo nix flake update ~/nixos"
        alias cleanup="sudo nix-collect-garbage -d"
-       alias rbs="sudo nixos-rebuild switch -I nixpkgs=/nix/var/nix/profiles/per-user/root/channels/nixos/nixpkgs -I nixos-config=/etc/nixos/configuration.nix"
-       alias spf="superfile"
 
        # fish colors
        set -U fish_color_normal f8f8f2
