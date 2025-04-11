ee5daf443608d6508787e0189940a4e74ac59420
Simpaqt <simpaqt@yahoo.com>
2025-04-11 13:25:57 +0200

    laptop

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -62,13 +62,16 @@
   # services.libinput.enable = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
-  # users.users.alice = {
-  #   isNormalUser = true;
-  #   extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
-  #   packages = with pkgs; [
-  #     tree
-  #   ];
-  # };
+  users.users.simpa-laptop = {
+    shell = pkgs.fish;
+    isNormalUser = true;
+    description = "simpa";
+    extraGroups = [ "networkmanager" "wheel" "gamemode" ];
+   # packages = with pkgs; [
+    #  kdePackages.kate
+    #  thunderbird
+   # ];
+  };
 
   # programs.firefox.enable = true;
 
