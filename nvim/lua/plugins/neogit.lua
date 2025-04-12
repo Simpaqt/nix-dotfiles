290767eb3c083b2b8a6197556b504dbed8946e96
Simpaqt <simpaqt@yahoo.com>
2025-04-12 16:56:58 +0200

    update

--- nvim/lua/plugins/neogit.lua
+++ nvim/lua/plugins/neogit.lua
@@ -1,11 +1,29 @@
 return {
   "NeogitOrg/neogit",
   dependencies = {
-    "nvim-lua/plenary.nvim",         -- required
-    "sindrets/diffview.nvim",        -- optional - Diff integration
-
-    -- Only one of these is needed.
-    "ibhagwan/fzf-lua",              -- optional
+    "nvim-lua/plenary.nvim", -- required
+    "sindrets/diffview.nvim", -- optional - Diff integration
+    "ibhagwan/fzf-lua", -- optional
   },
-  config = true
+  keys = {
+    {
+      "<leader>ng",
+      function()
+        require("neogit").open()
+      end,
+      desc = "Open Neogit",
+    },
+    {
+      "<leader>ns",
+      function()
+        require("neogit").open({ kind = "split" })
+      end,
+      desc = "Open Neogit in split",
+    },
+  },
+  config = function()
+    require("neogit").setup({
+      -- You can add any additional Neogit configuration options here if needed
+    })
+  end,
 }
